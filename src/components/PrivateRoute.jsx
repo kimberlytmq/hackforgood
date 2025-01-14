26f86fb446dafae0135d1de2c75a7622c6a90ec1
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2025-01-14 21:47:55 +0800

    Commit .env file new file:   .env.local modified:   .gitignore modified:   src/components/PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,10 @@
-import React from "react"
-import { Navigate } from "react-router-dom"
-import { useAuth } from "../contexts/AuthContext"
+import React from "react";
+
+import {  Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function PrivateRoute({ children }) {
-  const { currentUser } = useAuth()
+  const { currentUser } = useAuth();
 
-  return currentUser ? children : <Navigate to="/login" />
+  return currentUser ? children : <Navigate to="/login" />;
 }
