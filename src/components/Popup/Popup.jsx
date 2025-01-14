9be2fd6b4a98077a76b02a59cceb1a3630cc2605
Shruthiiii03 <muralishruthi222@gmail.com>
2025-01-14 22:27:32 +0800

    Update Add Meeting popup.css

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -8,14 +8,9 @@ function Popup({ setIsPopupOpen }) {
     <div className="popup">
       <div onClick={handleClose} className="overlay"></div>
       <div className="popup-content">
-        <h2>Hello Modal</h2>
+        <h2>Add Meeting</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          perferendis suscipit officia recusandae, eveniet quaerat assumenda id
-          fugit, dignissimos maxime non natus placeat illo iusto! Sapiente
-          dolorum id maiores dolores? Illum pariatur possimus quaerat ipsum
-          quos molestiae rem aspernatur dicta tenetur. Sunt placeat tempora
-          vitae enim incidunt porro fuga ea.
+          Choose the people you want to meet with and the time slots you're free!
         </p>
         <button className="close-popup" onClick={handleClose}>
           CLOSE
